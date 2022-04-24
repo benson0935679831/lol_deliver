48dbb69500d9f0f4c62ca6680df79e9ff809e948
Scott <benson891225@gmail.com>
2022-04-24 15:00:49 +0800

    Hello World

---
+++ app/src/main/java/com/example/lol_deliver/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lol_deliver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
