1e2423c9165051b76a513676196aa00ba63b79e4
BensonOUO <38932737+benson0935679831@users.noreply.github.com>
2022-05-02 17:52:10 +0800

    Add files via upload

---
+++ android/app/src/main/java/com/example/lol_deliver/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.lol_deliver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void goToLoginIntent(View view){
+        Intent intent = new Intent(this, UserLoginActivity.class);
+
+        startActivity(intent);
+    }
+}
